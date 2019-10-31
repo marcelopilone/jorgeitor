485c88a50f9e1e6c3dcde84978566d47a0918753
marcelopilone <mdpilone@gmail.com>
2019-10-31 11:29:38 -0300

    carga de estados

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
 ## material design
 from kivymd.theming import ThemeManager
 ## python
@@ -19,21 +20,37 @@ import configparser
 import sqlalchemy as db
 import json
 import pandas as pd
+import datetime
+## archivos python hechos por nosotros
+from database import *
 
 Config.set('graphics', 'width', 1600)
 Config.set('graphics', 'height', 800)
-
 
 class MainApp(App,BoxLayout):
     theme_cls = ThemeManager()
 
     def guardarEstado(self,val):
-        print(val)
-        pop = Popup(title='Invalid Form',
-            content=Label(text='Please fill in all inputs with valid information.'),
+        base = conexionBaseDeDatos()
+        engine = base.conexion
+        connection = engine.connect()
+        metadata = db.MetaData()
+        estados = db.Table('estados', metadata, autoload=True, autoload_with=engine)
+        fechaCreacion = datetime.datetime.now()
+        query = db.insert(estados).values(name=val,created=fechaCreacion) 
+        ResultProxy = connection.execute(query)
+        if( ResultProxy ):
+            pop = Popup(title='Estados',
+            content=Label(text='Se ha creado el estado con exito.'),
             size_hint=(None, None), size=(400, 400))
+            pop.open()
 
-        pop.open()
+class ver_estados(Screen):
+    pass
+
+#sm = ScreenManager()
+#sm.add_widget(ver_los_estados(name='ver_estados'))
+
 
 class screen1(Screen):	
     def build(self):
