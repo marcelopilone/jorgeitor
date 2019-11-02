0c6a76d597803b5c69b4318d4f278231984e1ff1
marcelopilone <mdpilone@gmail.com>
2019-11-02 16:09:06 -0300

    el boton de eliminar pero sin refrescar

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ Config.set('graphics', 'height', 800)
 class MainApp(App,BoxLayout,GridLayout):
     theme_cls = ThemeManager()
 
+    def probando():
+        print('asdasd')
+
+    jaja = probando()
+
+    
 
     def guardarEstado(self,val):
         if not val:
@@ -46,7 +52,7 @@ class MainApp(App,BoxLayout,GridLayout):
             connection = engine.connect()
             metadata = db.MetaData()
             estados = db.Table('estados', metadata, autoload=True, autoload_with=engine)
-            fechaCreacion = datetime.datetime.now()
+            fechaCreacion = datetime.now()
             query = db.insert(estados).values(name=val,created=fechaCreacion) 
             ResultProxy = connection.execute(query)
             if( ResultProxy ):
@@ -55,34 +61,29 @@ class MainApp(App,BoxLayout,GridLayout):
                 size_hint=(None, None), size=(400, 400))
                 pop.open()
 
-    
 
     '''
         Muestra los estados
     '''            
     def mostrarEstados(self, **kwargs):
-        ## Agrego las cabeceras de la tabla
-        self.root.ids.listado_de_estados.clear_widgets()
-        estadoName = Label(text='[color=000000]Nombre[/color]',font_size='20sp',markup = True,line_height = 150)
-        self.root.ids.listado_de_estados.add_widget(estadoName)
-        fechaDeCreacion = Label(text='[color=000000]Fecha creación[/color]',font_size='20sp',markup = True,line_height = 150)
-        self.root.ids.listado_de_estados.add_widget(fechaDeCreacion)
-        acciones = Label(text='[color=000000]Acciones[/color]',font_size='20sp',markup = True,line_height = 150)
-        self.root.ids.listado_de_estados.add_widget(acciones)
-        campoVacio = Label(text='[color=000000][/color]',font_size='20sp',markup = True,line_height = 150)
-        self.root.ids.listado_de_estados.add_widget(campoVacio)
 
+        ## declaro la conexion y la tabla estados que es la que voy a usar en esta funcion
         base = conexionBaseDeDatos()
         engine = base.conexion
         connection = engine.connect()
         metadata = db.MetaData()
         estados = db.Table('estados', metadata, autoload=True, autoload_with=engine)
-        query = db.select([estados])
-        ResultProxy = connection.execute(query)
-        ResultSet = ResultProxy.fetchall()
 
         def eliminarEstado(self,idEstado):
-            print(idEstado)
+            base = conexionBaseDeDatos()
+            engine = base.conexion
+            connection = engine.connect()
+            query = db.delete(estados)
+            query = query.where(estados.columns.id == idEstado)
+            results = connection.execute(query)
+            if(results):
+                self.root.ids.listado_de_estados.clear_widgets()
+                Clock.schedule_once(App.get_running_app().mostrarEstados)
 
         def editarEstado(self,idEstado):
             print(idEstado)
@@ -106,11 +107,26 @@ class MainApp(App,BoxLayout,GridLayout):
                 idDelEstado = x[0]
                 listarBotonesParaEditar(self,idDelEstado)
                 listarBotonesParaEliminar(self,idDelEstado)
-        
 
-        print(recorrerArray( ResultSet ))
-        
+        def mostrarTabla():
+            ## Agrego las cabeceras de la tabla
+            self.root.ids.listado_de_estados.clear_widgets()
+            estadoName = Label(text='[color=000000]Nombre[/color]',font_size='20sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_estados.add_widget(estadoName)
+            fechaDeCreacion = Label(text='[color=000000]Fecha creación[/color]',font_size='20sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_estados.add_widget(fechaDeCreacion)
+            acciones = Label(text='[color=000000]Acciones[/color]',font_size='20sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_estados.add_widget(acciones)
+            campoVacio = Label(text='[color=000000][/color]',font_size='20sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_estados.add_widget(campoVacio)
+            # traigo los datos            
+            query = db.select([estados])
+            ResultProxy = connection.execute(query)
+            ResultSet = ResultProxy.fetchall()
+            ## Se muestran las filas                        
+            print(recorrerArray( ResultSet ))
 
+        mostrarTabla()
 
         '''layoutGrid = GridLayout(size_hint=(.8,.8),pos_hint={"center_x":.5,"center_y":.5},orientation='vertical',cols=4 )
 
