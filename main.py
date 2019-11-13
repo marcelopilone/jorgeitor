02c7435511dc42a1c6eb1cb7f07cc3fd4aff64cd
marcelopilone <mdpilone@gmail.com>
2019-11-13 11:58:33 -0300

    abm de transferencias

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
@@ -29,8 +30,7 @@ from pprint import pprint # esto es como el debug que usamos en cakephp se usa p
 from database import *
 import os
 
-Config.set('graphics', 'width', 1600)
-Config.set('graphics', 'height', 800)
+Window.size = (1600, 1200)
 
 class MainApp(App,BoxLayout,GridLayout):
     theme_cls = ThemeManager()
@@ -180,6 +180,119 @@ class MainApp(App,BoxLayout,GridLayout):
 
         mostrarTabla()
 
+    '''
+        Muestra transferencias
+    '''            
+    def mostrarTransferencias(self, **kwargs):
+
+        ## declaro la conexion y la tabla estados que es la que voy a usar en esta funcion
+        base = conexionBaseDeDatos()
+        engine = base.conexion
+        connection = engine.connect()
+        metadata = db.MetaData()
+        transferencias = db.Table('transferencias', metadata, autoload=True, autoload_with=engine)
+
+        def eliminarTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia,botonEditar,botonEliminar,fechaTransCreacion):
+            base = conexionBaseDeDatos()
+            engine = base.conexion
+            connection = engine.connect()
+            query = db.delete(transferencias)
+            query = query.where(transferencias.columns.id == idTransferencia)
+            results = connection.execute(query)
+            if(results):
+                self.root.ids.listado_de_transferencias.remove_widget(fechaTrans)
+                self.root.ids.listado_de_transferencias.remove_widget(numeroTransferencia)
+                self.root.ids.listado_de_transferencias.remove_widget(fechaTrans)
+                self.root.ids.listado_de_transferencias.remove_widget(botonEditar)
+                self.root.ids.listado_de_transferencias.remove_widget(botonEliminar)
+                self.root.ids.listado_de_transferencias.remove_widget(fechaTransCreacion)
+
+        def edicionTransferencia(idTransferencia,transferenciaAnterior,val,val2,fechaTrans,numeroTransferencia,pop):
+            queryActualizar = db.update(transferencias).values(fecha = val,num_transferencia = val2).where(transferencias.columns.id == idTransferencia)
+            ResultProxy = connection.execute(queryActualizar)
+            if(ResultProxy):
+                fechaTrans.text = str('[color=000000]'+val+'[/color]')
+                numeroTransferencia.text = str('[color=000000]'+val2+'[/color]')
+                print('jaja')
+                pop.dismiss()
+
+        def editarTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia):
+
+
+            query = db.select([transferencias]).where(transferencias.columns.id == idTransferencia)
+            ResultProxy = connection.execute(query)
+            ResultSet = ResultProxy.fetchall()
+            transferenciaAnteriorFecha = ResultSet[0][1];
+            transferenciaAnteriorFecha = transferenciaAnteriorFecha.strftime("%Y-%m-%d")
+            numTransAnterior = ResultSet[0][2];
+
+            pop = Popup(title='Modificar transferencia',
+            size_hint=(None, None), size=(400, 400))
+
+            layout       = BoxLayout( orientation='vertical' )
+            labelFecha = Label(text = "Fecha transferencia")
+            inputEdicion = TextInput(id="txt_input_fecha_transferencia_edicion",text = transferenciaAnteriorFecha)
+            labelNumeroDeTransferencia = Label(text = "Número de transferencia")
+            inputEdicionNumTrans = TextInput(id="txt_input_num_transferencia_edicion",text = str(numTransAnterior))
+            botonParaActualizar = Button(text = "Actualizar",on_press=lambda x:edicionTransferencia(idTransferencia,transferenciaAnteriorFecha,inputEdicion.text,inputEdicionNumTrans.text,fechaTrans,numeroTransferencia,pop))
+
+            layout.add_widget(labelFecha)
+            layout.add_widget(inputEdicion)
+            layout.add_widget(labelNumeroDeTransferencia)
+            layout.add_widget(inputEdicionNumTrans)
+            layout.add_widget(botonParaActualizar)
+
+            pop = Popup(title='Modificar transferencia',
+            content= layout,
+            size_hint=(None, None), size=(400, 400))
+            pop.open()
+
+        def listarBotonesTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia,fechaTransCreacion):
+            botonEditar = Button(text='Editar',on_press = lambda x:editarTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia))
+            self.root.ids.listado_de_transferencias.add_widget(botonEditar)
+            botonEliminar = Button(text='Eliminar',on_press = lambda x:eliminarTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia,botonEditar))
+            botonEliminar = Button(text='Eliminar',on_press = lambda x:eliminarTransferencia(self,idTransferencia,fechaTrans,numeroTransferencia,botonEditar,botonEliminar,fechaTransCreacion))
+            self.root.ids.listado_de_transferencias.add_widget(botonEliminar)
+
+        def recorrerArrayTransferencias( arrayVar ):
+            for x in arrayVar[:20]:
+                fecha = x[1]
+                #fecha_final = fecha.strftime("%m/%d/%Y, %H:%M:%S")
+                fecha_final = fecha.strftime("%m/%d/%Y")
+                fechaTrans = Label(text='[color=000000]'+fecha_final+'[/color]',font_size='20sp',markup = True)
+                self.root.ids.listado_de_transferencias.add_widget(fechaTrans)
+                numeroTransferencia = Label(text='[color=000000]'+str(x[2])+'[/color]',font_size='20sp',markup = True)
+                self.root.ids.listado_de_transferencias.add_widget(numeroTransferencia)
+                fecha = x[3]
+                fecha_final = fecha.strftime("%m/%d/%Y")
+                fechaTransCreacion = Label(text='[color=000000]'+fecha_final+'[/color]',font_size='20sp',markup = True)
+                self.root.ids.listado_de_transferencias.add_widget(fechaTransCreacion)
+                
+                idDelEstado = x[0]
+                listarBotonesTransferencia(self,idDelEstado,fechaTrans,numeroTransferencia,fechaTransCreacion)
+
+        def mostrarTablaTransferencias():
+            ## Agrego las cabeceras de la tabla
+            self.root.ids.listado_de_transferencias.clear_widgets()
+            fechaTrans = Label(id='hola',text='[color=000000]Fecha transferencia[/color]',font_size='22sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_transferencias.add_widget(fechaTrans)
+            numeroTrans = Label(text='[color=000000]Número transferencia[/color]',font_size='22sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_transferencias.add_widget(numeroTrans)
+            fechaDeCreacion = Label(text='[color=000000]Fecha creación[/color]',font_size='22sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_transferencias.add_widget(fechaDeCreacion)            
+            acciones = Label(text='[color=000000]Acciones[/color]',font_size='22sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_transferencias.add_widget(acciones)
+            campoVacio = Label(text='[color=000000][/color]',font_size='22sp',markup = True,line_height = 150)
+            self.root.ids.listado_de_transferencias.add_widget(campoVacio)
+            # traigo los datos            
+            query = db.select([transferencias])
+            ResultProxy = connection.execute(query)
+            ResultSet = ResultProxy.fetchall()
+            ## Se muestran las filas                        
+            print(recorrerArrayTransferencias( ResultSet ))
+
+        mostrarTablaTransferencias()
+
 
 class ver_estados(Screen):
     pass
@@ -188,9 +301,9 @@ class ver_estados(Screen):
 #sm.add_widget(ver_los_estados(name='ver_estados'))
 
 
-class screen1(Screen):	
+class screen1(Screen):  
     def build(self):
-	       print("jaja 1")
+           print("jaja 1")
 
 if __name__ == '__main__':
     MainApp().run()
